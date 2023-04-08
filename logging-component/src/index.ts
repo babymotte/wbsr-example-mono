cc3c527e98ae3d54785c85c4ba5fa7fbbb6c47ba
Michael Bachmann <mbachmann@bbmsoft.net>
2023-04-08 14:10:39 +0200

    updated example

--- logging-component/src/index.ts
+++ logging-component/src/index.ts
@@ -1,5 +1,6 @@
 import { wbsrInitComponent, ModuleComponent, ServiceInstance } from "wbsr-js";
 import { component } from "./LoggingComponent";
-import module from "./component.json";
 
-wbsrInitComponent(module as ModuleComponent, component);
+const packageJson: ModuleComponent = require("../package.json");
+
+wbsrInitComponent(packageJson, component);
